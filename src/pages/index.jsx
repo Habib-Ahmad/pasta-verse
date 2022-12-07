dbc36717db98f5c5e0c109c416ace8350a34502a
Ahmad Habib <ahmadalkali69@gmail.com>
2022-12-07 10:52:11 +0100

    Project setup complete

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <p>hello world</p>
+    </div>
+  );
+};
+
+export default Home;
